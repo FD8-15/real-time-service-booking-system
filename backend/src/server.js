33fbdb035cddaf960a181885642b13d1e88edf83
FD8-15 <rajrnaik12@gmail.com>
2026-05-19 20:47:17 +0530

    fix: socket.io cors for production netlify

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,12 +4,11 @@ const onlineUsers = new Map();
 
 const initSocket = (server) => {
 
-    io = new Server(server, {
-        cors: {
-            origin: "http://127.0.0.1:5500",
-            credentials: true
-        }
-    });
+    const allowedOrigins = [
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
+        "https://nearbyservice24.netlify.app"
+    ];
 
     io.on("connection", (socket) => {
 
